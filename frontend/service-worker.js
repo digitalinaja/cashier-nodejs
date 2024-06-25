e859fa9441341aecac0fedd5d16cefb8b5c75e24
Sekret DQ <sekret@darulquran.sch.id>
2024-06-25 09:21:29 +0700

    notification click handler + caching mechanism

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,15 +1,68 @@
+const CACHE_NAME = 'cache-v2';
+const URLS_TO_CACHE = [
+    '/',
+    '/index.produk.html'
+];
+const API_URLS = [];
+
+
 self.addEventListener('install', event => {
-    console.log('ServiceWorker installed');
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => {
-          return response || fetch(event.request);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(URLS_TO_CACHE);
         })
-    );
-  });
+  );
+  console.log('ServiceWorker installed');
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+      caches.keys().then(cacheNames => {
+          return Promise.all(
+              cacheNames.map(cacheName => {
+                  if (!cacheWhitelist.includes(cacheName)) {
+                      return caches.delete(cacheName);
+                  }
+              })
+          );
+      })
+  );
+});
+  
+self.addEventListener('fetch', event => {
+  if (API_URLS.some(url => event.request.url.includes(url))) {
+      event.respondWith(
+          caches.match(event.request).then(async cachedResponse => {
+              if (cachedResponse) {
+                    return cachedResponse;
+                } else {
+                    const networkResponse = await fetch(event.request);
+                    const responseClone = networkResponse.clone();
+                    const modifiedResponse = new Response(responseClone.body, {
+                        status: responseClone.status,
+                        statusText: responseClone.statusText,
+                        headers: {
+                            ...responseClone.headers,
+                            'X-From-Cache': 'false'
+                        }
+                    });
+                    const cache = await caches.open(CACHE_NAME);
+                    await cache.put(event.request, modifiedResponse);
+                    return networkResponse;
+                }
+          })
+      );
+  } else {
+      event.respondWith(
+          caches.match(event.request).then(cachedResponse => {
+              return cachedResponse || fetch(event.request);
+          })
+      );
+  }
+});
 
 // Version check and update logic
 self.addEventListener('message', (event) => {
@@ -31,4 +84,25 @@ self.addEventListener('message', (event) => {
         })
     );
   }
+});
+
+//handle notification click
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close(); 
+  var fullPath = self.location.origin + event.notification.data.path; 
+  clients.openWindow(fullPath); 
+});
+
+//handle push event
+self.addEventListener('push', (event) => {
+  let data = {};
+  if (event.data) {
+      data = event.data.json();
+  }
+  const options = {
+    body: data.body || 'Default body'
+  };
+  event.waitUntil(
+    self.registration.showNotification(data.title || 'Default title', options)
+  );
 });
